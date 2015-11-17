4fe36981e8ffe94bf1240c48a17f5803ab699270
Robert Gravina <robert.gravina@kii.com>
2015-11-17 10:53:45 +0900

    Added some comments about looping through string.

--- src/Lexer.c
+++ src/Lexer.c
@@ -1,5 +1,18 @@
+#include <stdio.h>
+#include <string.h>
 #include "Lexer.h"
 
+/* An integer */
+struct tINTEGER {
+  int lineno;
+  char* value;
+};
+
 char* Lexer_lex(char* code) {
+  //int len = strlen(code);
+  //int current_pos = 0;
+  // for (int i = 0; i < len; i++) {
+  //   printf("%c", code[i]);
+  // }
   return code;
 }
