26c5776db404dc4835d79d5ddade7ad32ef75bd6
Robert Gravina <robert.gravina@gmail.com>
2015-11-23 13:25:04 +0900

    Refactoring - moved more of creating and adding token to methods.

--- src/Lexer.c
+++ src/Lexer.c
@@ -4,7 +4,8 @@
 #include <ctype.h>
 #include "lexer.h"
 
-Token* new_token();
+Token* new_token(char* code);
+void add_token(Token* token);
 
 void crb_init_lexer() {
   lexer = (Lexer*)malloc(sizeof(Lexer));
@@ -25,25 +26,29 @@ void crb_lexer_lex(char* code) {
     if (isdigit(code[i])) {
       lexer->curr_end_pos++;
     } else {
-      Token* token = new_token();
-      int token_length = lexer->curr_end_pos - lexer->curr_start_pos;
-      token->token->value = (char *)malloc(sizeof(char) * token_length+1);
-      token->token->value[token_length] = '\0';
-      strncpy(token->token->value, code+lexer->curr_start_pos, token_length);
-      lexer->curr_start_pos = lexer->curr_end_pos;
-      lexer->token = token;
-      lexer->num_tokens++;
+      Token* token = new_token(code);
+      add_token(token);
       break;
     }
   }
 }
 
-Token* new_token() {
+void add_token(Token* token) {
+  lexer->curr_start_pos = lexer->curr_end_pos;
+  lexer->token = token;
+  lexer->num_tokens++;
+}
+
+Token* new_token(char* code) {
   Token* token = (Token*)malloc(sizeof(Token));
   Integer* integer = (Integer*)malloc(sizeof(Integer));
   token->token = integer;
   token->start = lexer->curr_start_pos;
   token->lineno = lexer->curr_lineno;
   token->next = NULL;
+  int token_length = lexer->curr_end_pos - lexer->curr_start_pos;
+  token->token->value = (char *)malloc(sizeof(char) * token_length+1);
+  token->token->value[token_length] = '\0';
+  strncpy(token->token->value, code+lexer->curr_start_pos, token_length);
   return token;
 }
